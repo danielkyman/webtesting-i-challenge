33bb5698bf0705b57a905cfaa8d31f3474562e05
danielkyman <danny.kyman@gmail.com>
2020-04-22 16:36:58 -0700

    created testing layout - initial test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,15 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require("./enhancer.js");
 // test away!
+const testItem = { name: "item", durability: 50, enhancement: 10 };
+
+describe("enhancer.js", () => {
+  it("should run without errors", () => {
+    expect(true).toBe(true);
+  });
+  describe("repair", () => {
+    it("returns a new item with durability restored to 100", () => {
+      expect(repair(testItem).durability).toBe(100);
+    });
+  });
+  describe("");
+});
